b3f35edb9da1d8e8c98a4bb88a808792e49cb87a
flische <flische@gmail.com>
2018-08-09 12:05:33 -0700

    added todo list dummy data

---
+++ src/data/todo.js
@@ -0,0 +1,27 @@
+export default [
+    {
+        title: 'Find a concert',
+        details: 'go to concertbuddy.app and search for concerts',
+        _id: '001'
+    },
+    {
+        title: 'Create a trip board',
+        details: 'Create a board for chosen concert',
+        _id: '002'
+    },
+    {
+        title: 'Sign up to save info',
+        details: 'Sign up to save info and invite friends!',
+        _id: '003'
+    },
+    {
+        title: 'Invite friends',
+        details: 'Invite your friends by emailing them!',
+        _id: '004'
+    },
+    {
+        title: 'Assign responsibilities',
+        details: 'Assign responsibilities to friends!',
+        _id: '005'
+    },
+]
