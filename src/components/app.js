bb00f06dd16c83194167867ab6ef8871d5285007
flische <flische@gmail.com>
2018-08-10 23:19:20 -0700

    cleaned up code

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,9 @@
 import 'materialize-css/dist/css/materialize.min.css';
 import React, {Component} from 'react';
+import axios from 'axios';
 import AddItem from './add_item';
 import TodoList from './todo_list';
 import listData from '../data/todo';
-import axios from 'axios';
 
 const BASE_URL = 'http://api.reactprototypes.com';
 const API_KEY = '?key=c618_demofed';
@@ -13,7 +13,7 @@ class App extends Component{
         super(props);
 
         this.state = {
-            items: []
+            listData: listData
         }
     }
 
@@ -22,8 +22,6 @@ class App extends Component{
     }
 
     async addItem(item){
-        // we are expecting an item / object with "title" and "details"
-        // we need to use an object because we use the properties
         try {
             await axios.post(`${BASE_URL}/todos${API_KEY}`, item);
 
@@ -32,48 +30,39 @@ class App extends Component{
         catch(error){
             console.log('Something went wrong: ', error)
         }
-
-        // item._id = new Date().getTime(); // set the _id of each item to a time stamp!
-        //
-        // this.setState({
-        //     items: [item, ...this.state.items]
-        // });
     }
 
     async getListData(){
-        // this is where you would call the server for your data //
-        const response = await axios.get(`${BASE_URL}/todos${API_KEY}`);
+        try {
+            const response = await axios.get(`${BASE_URL}/todos${API_KEY}`);
 
-        this.setState({
-            items: response.data.todos
-        });
+            this.setState({
+                listData: response.data.todos
+            })
+        }
+        catch(error){
+            console.log('Error: ', error.message);
+        }
     }
 
-    // Above function using axios with async and await is the same as below//
-
-    // getListData(){
-    //     axios.get(`${BASE_URL}/todos${API_KEY}`).then( (response) => {
-    //             this.setState({
-    //                 items: response.data.todos
-    //             });
-    //     }).catch((error) => {
-    //         console.log('There was an error!:', error.message);
-    //     });
-    // }
+    async deleteItem(id){
+        const response = await axios.delete(`${BASE_URL}/todos/${id + API_KEY}`);
+        console.log("Response: ", response);
+        this.getListData();
+    }
 
     render(){
-
+        const {listData} = this.state;
         return (
             <div>
                 <div className="container">
                     <h1 className="center">To Do List</h1>
                     <AddItem add={this.addItem.bind(this)}/>
-                    <TodoList list={this.state.items}/>
+                    <TodoList list={listData} deleteItemCallback={this.deleteItem.bind(this)}/>
                 </div>
             </div>
-        );
+        )
     }
 }
-
 
 export default App;
--- src/components/todo_list.js
+++ src/components/todo_list.js
@@ -1,10 +1,23 @@
 import React, {Component} from 'react';
 
 class TodoList extends Component {
+    constructor(props){
+        super(props);
+    }
     render(){
 
-        const listElements = this.props.list.map( (item) => {// 'list' named in app.js
-            return <li className="collection-item" key={item._id}>{item.title}</li>
+        const listElements = this.props.listData.map( (item) => {
+            return (
+                <li className="collection-item" key={item._id}>
+                    <div className="col s10">
+                        {item.title}
+                    </div>
+                    <button className="btn red darken-2 right-align"
+                            onClick={(event)=> this.props.deleteItem(item._id)}
+                    >Delete
+                    </button>
+                </li>
+            )
         });
 
         return(
